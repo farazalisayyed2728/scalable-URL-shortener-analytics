0a019f30a874150a89f04dc89952d8d8125a0590
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-24 12:52:26 +0530

    modified config urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from apps.links.views import RedirectShortURLView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.links.urls")),
+
+re_path(
+    r"^(?P<short_code>[A-Za-z0-9_-]{3,32})/?$",
+    RedirectShortURLView.as_view(),
+    name="short-url-redirect",
+    ),
 ]
