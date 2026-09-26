cfd90a77d368aa0930e93018eb2a8d7ba2dd0f04
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-26 12:24:07 +0530

    update urls

--- apps/links/urls.py
+++ apps/links/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import ShortURLCreateAPIView
 
+from .views import (
+    ShortURLCreateAPIView,
+    ShortURLDetailUpdateDeleteAPIView,
+)
+
 urlpatterns = [
     path("urls/", ShortURLCreateAPIView.as_view(), name="url-create"),
+    path("urls/<str:short_code>/", ShortURLDetailUpdateDeleteAPIView.as_view(), name="url-detail-update-delete"),
 ]
